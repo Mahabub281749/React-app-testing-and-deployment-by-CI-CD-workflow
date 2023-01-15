ceb74c79bd6b8543f55bed49ff5bb96d7e3de918
parvez <mahabubhasan.austbd@gmail.com>
2023-01-15 21:41:33 +0200

    Update release config file

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	branches: "master",
-	repositoryUrl: "https://github.com/alialaa/react-app",
+	repositoryUrl: "https://github.com/Mahabub281749/React-app-testing-and-deployment-by-CI-CD-workflow/blob/Semantic-Release-in-Workflow",
 	plugins: [
 		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
 		"@semantic-release/github"
