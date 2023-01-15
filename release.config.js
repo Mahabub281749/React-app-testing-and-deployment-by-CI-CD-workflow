fac6d0e4d6f5641f223c9a50cf4898cbafc2070b
parvez <mahabubhasan.austbd@gmail.com>
2023-01-15 21:43:28 +0200

    Update Release config file

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	branches: "master",
-	repositoryUrl: "https://github.com/Mahabub281749/CI-CD-workflow-to-Automate-testing-and-deployment-of-a-React-App",
+	repositoryUrl: "https://github.com/Mahabub281749/React-app-testing-and-deployment-by-CI-CD-workflow",
 	plugins: [
 		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
 		["@semantic-release/github", {
